bb6a8d26f63317b1dd35f23e8f086c6c55102da0
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-07-05 10:08:07 -0300

    Conta Corrente e Poupança

--- bytebankHerdadoConta/src/bytebankHerdadoConta/ContaCorrente.java
+++ bytebankHerdadoConta/src/bytebankHerdadoConta/ContaCorrente.java
@@ -2,7 +2,18 @@ package bytebankHerdadoConta;
 
 public class ContaCorrente extends Conta {
 
+	public ContaCorrente() {
+		
+	}
+	
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
 	}
+	
+	@Override
+	public boolean saca(double valor) {
+		double valorASacar = valor + 0.2;
+		return super.saca(valorASacar);
+	}
 }
+	
--- bytebankHerdadoConta/src/bytebankHerdadoConta/ContaPoupanca.java
+++ bytebankHerdadoConta/src/bytebankHerdadoConta/ContaPoupanca.java
@@ -1,6 +1,11 @@
 package bytebankHerdadoConta;
 
 public class ContaPoupanca extends Conta {
+	
+	public ContaPoupanca() {
+		
+	}
+	
 	public ContaPoupanca(int agencia, int numero) {
 		super(agencia, numero);
 	}
