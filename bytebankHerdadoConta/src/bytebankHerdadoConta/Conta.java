67ad9c039f0a0b72db432b6be0ac042f3aa7b03b
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-07-05 13:14:39 -0300

    Abstract no exemplo Conta

--- bytebankHerdadoConta/src/bytebankHerdadoConta/Conta.java
+++ bytebankHerdadoConta/src/bytebankHerdadoConta/Conta.java
@@ -1,8 +1,8 @@
 package bytebankHerdadoConta;
 
-public class Conta {
+public abstract class Conta {
 
-	private double saldo;
+	protected double saldo;
 	private int agencia;
 	private int numero;
 	private Cliente titular;
@@ -21,9 +21,7 @@ public class Conta {
 //		System.out.println("Estou criando uma conta " + this.numero);
 	}
 
-	public void deposita(double valor) {
-		this.saldo = this.saldo + valor;
-	}
+	public abstract void deposita(double valor);
 
 	public boolean saca(double valor) {
 		if (this.saldo >= valor) {
--- bytebankHerdadoConta/src/bytebankHerdadoConta/ContaCorrente.java
+++ bytebankHerdadoConta/src/bytebankHerdadoConta/ContaCorrente.java
@@ -3,17 +3,21 @@ package bytebankHerdadoConta;
 public class ContaCorrente extends Conta {
 
 	public ContaCorrente() {
-		
+
 	}
-	
+
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
 	}
-	
+
 	@Override
 	public boolean saca(double valor) {
 		double valorASacar = valor + 0.2;
 		return super.saca(valorASacar);
 	}
+
+	@Override
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
 }
-	
